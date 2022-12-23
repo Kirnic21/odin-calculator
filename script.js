2d864bb047a74910912dab8fffdbabaf6fb23918
Kirnic21 <marcelokenji12121@gmail.com>
2022-12-23 14:35:45 -0300

    initial HTML and CSS

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-function sum(a,b)
-{
-    let sum = a + b
-    return sum
-}
-function subtract(a,b)
-{
-    let subtract = a - b
-    return subtract
-}
-function multiply(a,b)
-{
-    let multiply= a*b
-    return multiply
-}
-function divide(a,b)
-{
-    let divide = a/b
-    return divide
-}
 function operate(a,b,operator)
 {
-    if(operator === "/")
+    if (operator = "+")
     {
-    divide(a,b)
+        return a+b
     }
-    else if(operator === "*")
+    else if (operator = "-")
     {
-    multiply(a,b)
+        return a-b
     }
-    else if(operator === "-")
+    else if (operator = "/")
     {
-    subtract(a,b)
+        return a/b
     }
-    else if(operator === "+")
+    else if (operator = "*")
     {
-    sum(a,b)
+        return a*b
     }
 }
-let btns = document.querySelectorAll('.number');
+function display()
+{
 
-for (i of btns) {
-  i.addEventListener('click', function() {
-   document.querySelector("#hello").innerHTML = this.innerHTML;
-  });
+    
 }
+addEventListner("click",function(){
+    let 
+})
