bd4efc7515204bc58f89d1d49f1377877dc571a4
Kirnic21 <marcelokenji12121@gmail.com>
2022-12-26 11:26:41 -0300

    created display function

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+//operator function(oprate two give numbers)
 function operate(a,b,operator)
 {
-    if (operator = "+")
+    if (operator === "+")
     {
-        return a+b
+        return parseInt(a)+parseInt(b)
     }
-    else if (operator = "-")
+    else if (operator === "-")
     {
         return a-b
     }
-    else if (operator = "/")
+    else if (operator === "/")
     {
         return a/b
     }
-    else if (operator = "*")
+    else if (operator === "*")
     {
         return a*b
     }
 }
-function display()
-{
-
-    
-}
-addEventListner("click",function(){
-    let 
+const btn = document.querySelectorAll(".key-number")
+const display =document.querySelector("#display")
+//when click in a button,it shows it value on display
+btn.forEach((button)=>{
+    button.addEventListener("click",function pizza()
+    {
+        let result = operate(this.textContent , this.textContent, "+")
+        display.textContent = result
+    })
 })
