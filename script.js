fc1bfbb81b5486129918119489f8c06df13d9141
Kirnic21 <marcelokenji12121@gmail.com>
2022-12-27 15:45:06 -0300

    redoing the project

--- script.js
+++ script.js
@@ -23,25 +23,3 @@ function operate(a,b,operator)
         return wtfBOOOM
     }
 }
-const calculator = document.querySelector(".calculator")
-const keys = document.querySelector(".calculator-keys")
-const btn = document.querySelectorAll(".key-number");
-let symbol = null
-let number = " "
-let firstValue = null
-let secondValue = null
-
-function operateDisplay()
-    {   //conctanate into the display
-        number += this.textContent;
-        display.textContent=number;
-    }
-//when click in a button,it shows it value on display
-btn.forEach((button)=>{
-    button.addEventListener("click",operateDisplay)
-})
-operator.forEach((operation)=>{
-    operation.addEventListener("click",operateDisplay)
-    operation.addEventListener("click",handleOperation)
-})
-//equal number(show the result)
