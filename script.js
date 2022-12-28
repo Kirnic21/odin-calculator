004b400c1526ab960ac327f6964f28c6bcc2d0ca
Kirnic21 <marcelokenji12121@gmail.com>
2022-12-28 18:03:07 -0300

    new directory

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function operate(a,b,operator)
     {
         return parseInt(a)-parseInt(b)
     }
+    else if (operator === "/" && b === "0")
+    {
+        return "WTFBOOM"
+    }
     else if (operator === "/")
     {
         return parseInt(a)/parseInt(b)
@@ -18,8 +22,66 @@ function operate(a,b,operator)
     {
         return parseInt(a)*parseInt(b)
     }
-    else if(operator === "/" && b === 0)
+    
+}
+//DOM varibles
+const btn = document.querySelectorAll(".keyNumber")
+const display =document.querySelector(".calculator-display")
+const clear = document.querySelector(".clear")
+const operator = document.querySelectorAll(".operator")
+const equal = document.querySelector(".equal")
+let symbol = " "
+let result = " "
+let valueInDisplay = " "
+let value1 = " "
+function operateDisplay()
+    {   //conctanate into the display
+        valueInDisplay += this.textContent;
+        display.textContent=valueInDisplay;
+    }
+function operatorsChoice()
+{
+    if (this.textContent === "+")
     {
-        return wtfBOOOM
+            value1 = valueInDisplay
+            symbol = "+"
+            valueInDisplay = ""
+
+    }
+    else if(this.textContent ==="-")
+    {
+        value1 = valueInDisplay
+        symbol = "-"
+        valueInDisplay = ""
+    }
+    else if(this.textContent ==="/")
+    {
+        value1 = valueInDisplay
+        valueInDisplay = ""
+        symbol = "/"
+    }
+    else if(this.textContent ==="*")
+    {
+        value1 = valueInDisplay
+        valueInDisplay = ""
+        symbol = "*"
     }
 }
+//when click in a button,it shows it value on display
+btn.forEach((button)=>
+button.addEventListener("click",operateDisplay))
+//choose operators
+
+operator.forEach((operation)=>
+operation.addEventListener("click",operatorsChoice))
+equal.addEventListener("click",()=>{
+    result = operate(value1,valueInDisplay,symbol)
+    symbol = ""
+    valueInDisplay = result;
+    display.textContent = result
+})
+clear.addEventListener("click",()=>{
+    value1=""
+    valueInDisplay=""
+    symbol=""
+})
